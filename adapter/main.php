a4a8defec6f09c1e512edac4a71f39869511ef7d
acio-o9 <aco6280113@gmail.com>
2020-09-19 20:45:13 +0900

    [doc] main class dependents on PrintOut interface

--- adapter/main.php
+++ adapter/main.php
@@ -6,6 +6,10 @@ use App\PrintOutBanner;
 
 $p = new PrintOutBanner("Hello");
 
+/**
+ * main class dependes on PrintOut interface.
+ * printOutWeak, printOutStrong method are implemented with PrintOut interface.
+ */
 echo $p->printOutWeak();
 echo $p->printOutStrong();
 
