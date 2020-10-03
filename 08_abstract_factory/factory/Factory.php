23400f19d8b4cdb54484cafe6a134600afe1cdea
acio-o9 <aco6280113@gmail.com>
2020-10-03 17:14:27 +0900

    [modify] to Factory interface

--- 08_abstract_factory/factory/Factory.php
+++ 08_abstract_factory/factory/Factory.php
@@ -2,20 +2,9 @@
 
 namespace Factory;
 
-abstract class Factory {
-    public static function getFactory(string $classname): Factory
-    {
-        $factory = null;
-        try {
-            $factory = new $classname;
-        } catch (\Exception $e) {
-            echo $e->getMessage();
-        }
-        return $factory;
-    }
-
-    public abstract function createLink(string $caption, string $url): Link;
-    public abstract function createTray(string $caption): Tray;
-    public abstract function createPage(string $title, string $author): Page;
+interface Factory {
+    public function createLink(string $caption, string $url): Link;
+    public function createTray(string $caption): Tray;
+    public function createPage(string $title, string $author): Page;
 }
 
